00f8ca596e31250bc6b53a7ca8d2e6d7863d0815
inside <inside@inside.com>
2016-05-14 04:03:43 +0300

    question action

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,10 +1,14 @@
 <?php
-
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Question;
+use AppBundle\Form\Type\MultipleAnswerForm;
+use AppBundle\Form\Type\SingleAnswerForm;
+use AppBundle\Form\Type\TextAnswerForm;
+use AppBundle\Form\Type\VariantAnswerForm;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,8 +18,80 @@ class DefaultController extends Controller
         $testRepo = $em->getRepository("AppBundle:Test");
         $tests = $testRepo->findNewest(5);
 
-        return $this->render('default/index.html.twig', [
-            'tests' => $tests
-        ]);
+        return $this->render('default/index.html.twig', ['tests' => $tests]);
+    }
+
+    public function prefaceAction($id)
+    {
+
+    }
+
+    public function questionAction($testId, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $um = $this->get('user_manager');
+        $testService = $this->get('test_service');
+
+        $test = $em->find('AppBundle:Test', $testId);
+        if (!$test) {
+            throw $this->createNotFoundException('Теста под таким номером не существует');
+        }
+
+        $response = new Response();
+        if (!$user = $um->getUser($request)) {
+            $user = $um->createGuestUser();
+            $um->loginGuestUser($response, $user);
+        }
+
+        $attempt = $testService->continueAttemptOrStartNewOne($test, $user);
+        $question = $testService->getNextQuestion($attempt);
+
+        if (!$question) {
+            return $this->redirectToRoute('result', ['testId' => $testId]);
+        }
+
+        $form = $this->resolveForm(
+            $question,
+            ['attempt_id' => $attempt->getId()]
+        );
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $answerRepo = $em->getRepository('AppBundle:Answer');
+            $answerRepo->populateAndSave($form->getData());
+            return $this->redirectToRoute('question', ['testId' => $testId]);
+        }
+
+        return $this->render('default/question.html.twig', [
+            'form' => $form->createView(),
+            'question' => $question,
+        ], $response);
+    }
+
+    public function resultAction($testId)
+    {
+        return new Response("Test completed");
+    }
+
+    private function resolveForm(Question $question, array $data)
+    {
+        $data['question_id'] = $question->getId();
+        $data['choices'] = $question->getChoices();
+
+        switch ($question->getType()) {
+            case Question::TEXT:
+            case Question::DECIMAL:
+                return $this->createForm(TextAnswerForm::class, $data);
+            case Question::SINGLE:
+                $data['expanded'] = true;
+                $data['multiple'] = false;
+                return $this->createForm(VariantAnswerForm::class, $data);
+            case Question::MULTIPLE:
+                $data['expanded'] = true;
+                $data['multiple'] = true;
+                return $this->createForm(VariantAnswerForm::class, $data);
+            default:
+                return null;
+        }
     }
 }
