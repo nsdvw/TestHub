513f4842402edca186ac99aa3326e42e346ac359
inside <inside@inside.com>
2016-05-14 04:03:07 +0300

    helper class for userManager service

---
+++ src/AppBundle/Helper/StringGenerator.php
@@ -0,0 +1,46 @@
+<?php
+namespace AppBundle\Helper;
+
+/**
+ * Class StringGenerator
+ * @package AppBundle\Helper
+ */
+class StringGenerator
+{
+    /**
+     * Length of generated string
+     */
+    const LENGTH = 40;
+
+    /**
+     * @return string
+     */
+    private static function getCharacters()
+    {
+        return '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        . '~`!@#$%^&*()-=+_][{}|?><';
+    }
+
+    /**
+     * @return string
+     */
+    public static function generateToken()
+    {
+        $string = self::generateString();
+        return sha1($string);
+    }
+
+    /**
+     * @return string
+     */
+    public static function generateString()
+    {
+        $string = '';
+        $characters = self::getCharacters();
+        $charactersLength = strlen($characters);
+        for ($i = 0; $i < self::LENGTH; $i++) {
+            $string .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $string;
+    }
+}
