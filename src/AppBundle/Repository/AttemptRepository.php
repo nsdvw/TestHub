3d9db5e4fa391ab86087b95d394d3d7e17161794
inside <inside@inside.com>
2016-05-14 03:57:24 +0300

    extend AttemptRepository

--- src/AppBundle/Repository/AttemptRepository.php
+++ src/AppBundle/Repository/AttemptRepository.php
@@ -1,6 +1,9 @@
 <?php
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\Attempt;
+use AppBundle\Entity\Test;
+use AppBundle\Entity\User;
 use Doctrine\ORM\EntityRepository;
 
 /**
@@ -8,4 +11,26 @@ use Doctrine\ORM\EntityRepository;
  */
 class AttemptRepository extends EntityRepository
 {
+    /**
+     * @param Test $test
+     * @param User $user
+     * @return null|Attempt
+     */
+    public function findLastAttempt(Test $test, User $user)
+    {
+        return $this->findOneBy(
+            ['test' => $test->getId(), 'trier' => $user->getId()],
+            ['started' => 'DESC']
+        );
+    }
+
+    /**
+     * @param Attempt $attempt
+     */
+    public function save(Attempt $attempt)
+    {
+        $em = $this->getEntityManager();
+        $em->persist($attempt);
+        $em->flush();
+    }
 }
