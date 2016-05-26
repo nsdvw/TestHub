3505fc8c7594bc57c94e78dc938b666b97e3059a
inside <inside@inside.com>
2016-05-26 22:37:28 +0300

    calculator: refactoring, maxMark method, tests

--- src/TestHubBundle/Service/Calculator.php
+++ src/TestHubBundle/Service/Calculator.php
@@ -3,6 +3,7 @@ namespace TestHubBundle\Service;
 
 use TestHubBundle\Entity\Attempt;
 use TestHubBundle\Entity\Question;
+use TestHubBundle\Entity\Test;
 
 class Calculator
 {
@@ -13,59 +14,112 @@ class Calculator
         $userAnswers = $attempt->getAnswers();
 
         foreach ($questions as $question) {
-            $qid = $question->getId();
-            $userAnswersOnQuestion = [];
-            foreach ($userAnswers as $a) {
-                if ($a->getQuestion()->getId() === $qid) {
-                    $userAnswersOnQuestion[] = $a;
-                }
-            }
-            if (empty($userAnswersOnQuestion)) {
+            $answers = $this->getUserAnswersOnQuestion($userAnswers, $question);
+            if (empty($answers)) {
                 continue;
             }
-            if ($question->getType() === Question::TEXT) {
-                $correctAnswer = $question->getAnswerText();
-                if ($correctAnswer != $userAnswersOnQuestion[0]->getTextAnswer()) {
-                    continue;
-                }
-                $mark += $question->getPoints();
-            } elseif ($question->getType() === Question::DECIMAL) {
-                $correctAnswer = $question->getAnswerDecimal();
-                if ($correctAnswer != $userAnswersOnQuestion[0]->getDecimalAnswer()) {
-                    continue;
-                }
-                $mark += $question->getPoints();
-            } elseif ($question->getType() === Question::SINGLE) {
-                $variants = $question->getVariants();
-                $correctAnswer = null;
-                foreach ($variants as $v) {
-                    if ($v->getIsRight() === 'yes') {
-                        $correctAnswer = $v->getId();
-                        break;
-                    }
-                }
-                if ($correctAnswer != $userAnswersOnQuestion[0]->getVariant()->getId()) {
-                    continue;
-                }
-                $mark += $question->getPoints();
-            } elseif ($question->getType() === Question::MULTIPLE) {
-                $variants = $question->getVariants();
-                $correctAnswers = [];
-                foreach ($variants as $v) {
-                    if ($v->getIsRight() === 'yes') {
-                        $correctAnswers[] = $v->getId();
-                    }
-                }
-                $userVariantsIds = [];
-                foreach ($userAnswersOnQuestion as $a) {
-                    $userVariantsIds[] = $a->getVariant()->getId();
-                }
-                if (array_diff($correctAnswers, $userVariantsIds)) {
-                    continue;
-                }
-                $mark += $question->getPoints();
+            $mark += $this->accruePoints($question, $answers);
+        }
+
+        return $mark;
+    }
+
+    public function calculateMaxMark(Test $test)
+    {
+        $maxMark = 0;
+
+        foreach ($test->getQuestions() as $question) {
+            $maxMark += $question->getPoints();
+        }
+
+        return $maxMark;
+    }
+
+    private function getUserAnswersOnQuestion($userAnswers, Question $question)
+    {
+        $userAnswersOnQuestion = [];
+        foreach ($userAnswers as $a) {
+            if ($a->getQuestion()->getId() === $question->getId()) {
+                $userAnswersOnQuestion[] = $a;
             }
         }
-        return $mark;
+        return $userAnswersOnQuestion;
+    }
+
+    private function accruePoints(Question $question, $answers)
+    {
+        $points = 0;
+
+        if ($question->getType() === Question::TEXT) {
+            $points += $this->checkTextAnswer($question, $answers);
+        } elseif ($question->getType() === Question::DECIMAL) {
+            $points += $this->checkDecimalAnswer($question, $answers);
+        } elseif ($question->getType() === Question::SINGLE) {
+            $points += $this->checkSingleVariantAnswer($question, $answers);
+        } elseif ($question->getType() === Question::MULTIPLE) {
+            $points += $this->checkMultipleVariantAnswer($question, $answers);
+        }
+
+        return $points;
+    }
+
+    private function checkTextAnswer(Question $question, $answers)
+    {
+        $correctAnswer = $question->getAnswerText();
+        if ($correctAnswer === $answers[0]->getTextAnswer()) {
+            return $question->getPoints();
+        }
+
+        return 0;
+    }
+
+    private function checkDecimalAnswer(Question $question, $answers)
+    {
+        $correctAnswer = $question->getAnswerDecimal();
+        if ($correctAnswer === $answers[0]->getDecimalAnswer()) {
+            return $question->getPoints();
+        }
+
+        return 0;
+    }
+
+    private function checkSingleVariantAnswer(Question $question, $answers)
+    {
+        $variants = $question->getVariants();
+        $correctAnswer = null;
+        foreach ($variants as $v) {
+            if ($v->getIsRight() === 'yes') {
+                $correctAnswer = $v->getId();
+                break;
+            }
+        }
+        if ($correctAnswer === $answers[0]->getVariant()->getId()) {
+            return $question->getPoints();
+        }
+
+        return 0;
+    }
+
+    private function checkMultipleVariantAnswer(Question $question, $answers)
+    {
+        $variants = $question->getVariants();
+        $correctAnswers = [];
+
+        foreach ($variants as $v) {
+            if ($v->getIsRight() === 'yes') {
+                $correctAnswers[] = $v->getId();
+            }
+        }
+
+        $userVariantsIds = [];
+        foreach ($answers as $a) {
+            $userVariantsIds[] = $a->getVariant()->getId();
+        }
+
+        if (!array_diff($correctAnswers, $userVariantsIds)) {
+            return $question->getPoints();
+        }
+
+        return 0;
     }
 }
--- tests/TestHubBundle/Service/CalculatorTest.php
+++ tests/TestHubBundle/Service/CalculatorTest.php
@@ -1,20 +1,42 @@
 <?php
 namespace Tests\TestHubBundle\Service;
 
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use TestHubBundle\Service\Calculator;
-use Tests\TestHubBundle\TestCase;
 
-class CalculatorTest extends TestCase
+class CalculatorTest extends WebTestCase
 {
+    protected $em;
+
+    protected function setUp()
+    {
+        $client = static::createClient();
+
+        $app = new Application($client->getKernel());
+        $app->setAutoExit(false);
+
+        $this->em =
+            $app->getKernel()->getContainer()->get('doctrine')->getManager();
+    }
+
     public function testCalculateMark()
     {
-        $container = $this->getApplication()->getKernel()->getContainer();
-        $em = $container->get('doctrine')->getManager();
-        $attempt = $em->find('TestHubBundle:Attempt', 1);
+        $attempt = $this->em->find('TestHubBundle:Attempt', 1);
 
         $calculator = new Calculator();
         $mark = $calculator->calculateMark($attempt);
         $this->assertInternalType('integer', $mark);
         $this->assertEquals(15, $mark);
+    }
+
+    public function testCalculateMaxMark()
+    {
+        $test = $this->em->find('TestHubBundle:Test', 1);
+
+        $calculator = new Calculator();
+        $maxMark = $calculator->calculateMaxMark($test);
+        $this->assertInternalType('integer', $maxMark);
+        $this->assertEquals(30, $maxMark);
     }
 }
